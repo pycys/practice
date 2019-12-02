10cbf5e1a13bf83feb5ee37c9523f67263f951cb
pycys <51740642+pycys@users.noreply.github.com>
2019-12-03 04:32:02 +0900

    Update movie_download.py

--- movie_download.py
+++ movie_download.py
@@ -3,5 +3,5 @@ from pytube import YouTube
 url = input("ダウンロードしたい動画のURLは？　")
 for i in YouTube(url).streams.all():
     print(i)
-YouTube(url).streams.get_by_itag(int(input("どのtagにする？　"))).download(r"C:\Users\n1290380.STCN2\Downloads")
+YouTube(url).streams.get_by_itag(int(input("どのtagにする？　"))).download()
 print("ダウンロード完了！")
