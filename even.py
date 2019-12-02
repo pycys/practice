a4e7f2331b6d838707b923cb5e0c6f49a9350ca0
pycys <bjto1290380@gn.iwasaki.ac.jp>
2019-12-03 02:11:56 +0900

    ちょい改良（改悪）

--- even.py
+++ even.py
@@ -1,10 +1,9 @@
 string = "def even_judgment(i):\n"
-true = """    if i == {}:
-        return True\n"""
-false = """    if i == {}:
-        return False\n"""
+judgment = """    if i == {}:
+        return {}\n"""
 for i in range(int(input())+1):
-    string += true.format(i) if i%2 == 0 else false.format(i)
+    judge = i%2 == 0
+    string += judgement.format(i, judge) if judge else judgement.format(i, judge)
 string += """    return '知らん'\n"""
 with open("./even_judgment.py", mode="w") as f:
     f.write(string)
