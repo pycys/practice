5f5b637d5315a64a5d90d347fc0c9c147076e855
pycys <51740642+pycys@users.noreply.github.com>
2021-07-01 17:30:04 +0900

    Update even.py

--- even.py
+++ even.py
@@ -3,6 +3,6 @@ judgment = """    if i == {}:
         return {}\n"""
 for i in range(int(input())+1):
     string += judgment.format(i, i%2 == 0)
-string += """    return '知らん'\n"""
+string += """    return None\n"""
 with open("./even_judgment.py", mode="w") as f:
     f.write(string)
